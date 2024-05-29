4f907179f163b6804a4abf3e50200b0c3a01edcb
MatheusFuscaldi <99296660+matheusfgs@users.noreply.github.com>
2024-05-29 12:04:58 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1716994935,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/backgroundtile.png",
+		"images/player-sheet0.png",
+		"images/solidtile.png",
+		"images/jumpthrutile.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
